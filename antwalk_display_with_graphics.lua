e5f83aa2a79ee2c7f97b1eabb6694894624d16ba
Robert Curtin <robertecurtin@gmail.com>
2018-02-10 22:14:33 -0500

    Updated graphics library

--- antwalk_display_with_graphics.lua
+++ antwalk_display_with_graphics.lua
@@ -1,5 +1,5 @@
 local AntWalk = require('antwalk')
-local Board = require 'lib/lua-board-display/src/board'
+local DisplayGrid = require 'lib/lua-board-display/src/DisplayGrid'
 
 local count_x = 16
 local count_y = 12
@@ -10,26 +10,26 @@ local image_config = {
   count_x = count_x,
   count_y = count_y
 }
-local delay = true
-local board_display = Board(image_config, 200)
+
+local display_grid = DisplayGrid(image_config, 100)
 local ant_board = {
   location = {x=0, y=0},
   direction = 'north'
 }
 
-board_display.run(function()
+display_grid.run(function()
   ant_board = AntWalk(ant_board)
   for i = 1, count_x do
     for j = 1, count_y do
-      local x = i - ( count_x / 2 )
-      local y = j - ( count_y / 2 )
+      local x = i - math.floor(count_x / 2)
+      local y = j - math.floor(count_y / 2)
       if ant_board[x .. ',' .. y] then
-        board_display.display_image(i, j, 'images/black.png')
+        display_grid.display_image(i, j, 'images/black.png')
       else
-        board_display.display_image(i, j, 'images/white.png')
+        display_grid.display_image(i, j, 'images/white.png')
       end
       if x == ant_board.location.x and y == ant_board.location.y then
-        board_display.display_image(i, j, 'images/ant_' .. ant_board.direction .. '.png')
+        display_grid.display_image(i, j, 'images/ant_' .. ant_board.direction .. '.png')
       end
     end
   end
